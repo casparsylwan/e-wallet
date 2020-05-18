32e751c900cd42a2522fd9cb867485aa7c04768f
casparsylwan <casparsylwan@gmail.com>
2020-05-18 16:21:17 +0200

    Main structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HomeMain></HomeMain>
+    
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomeMain from '../components/pages/HomeMain'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HomeMain
   }
 }
 </script>
