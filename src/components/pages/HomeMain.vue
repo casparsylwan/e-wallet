ba4e663f06bc47e50570157092c26d808a3a0b85
casparsylwan <casparsylwan@gmail.com>
2020-05-19 09:54:18 +0200

    Transformed svg to componets

--- src/components/pages/HomeMain.vue
+++ src/components/pages/HomeMain.vue
@@ -1,13 +1,34 @@
 <template>
     <div class="homeMain">
-
+        Caspar
+        <ChipDark></ChipDark>
+        <Bitcoin></Bitcoin>
+        <BlockChain></BlockChain>
+        <Evil></Evil>
+        <Ninja></Ninja>
+        <ChipLight></ChipLight>
     </div>   
 </template>
 <script>
+import ChipDark from '../imgSVG/ChipDark';
+import Bitcoin from '../imgSVG/VendorBitcoin';
+import BlockChain from '../imgSVG/VendorBlockchain';
+import Evil from '../imgSVG/VendorEvil';
+import Ninja from '../imgSVG/VendorNinja';
+import ChipLight from '../imgSVG/ChipLight';
+
 export default {
+     components:{
+        ChipDark,
+        ChipLight,
+        Bitcoin,
+        BlockChain,
+        Evil,
+        Ninja
+    }
     
 }
 </script>
-<style lang="stylus" scoped>
+<style lang="scss" scoped>
 
 </style>
